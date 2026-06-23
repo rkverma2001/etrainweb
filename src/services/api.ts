8f9d9ec7834727393bb1e5e72823946a6451533e
rkverma2001 <riteshkumarverma2018@gmail.com>
2026-06-23 16:17:46 +0530

    updated

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const instance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "/api/v1",
   timeout: 5000,
 });
 
