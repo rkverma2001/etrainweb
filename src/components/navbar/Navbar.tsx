c3ee3f727ec724ce29b93e9efe9c97abcd5ae25e
rkverma2001 <riteshkumarverma2018@gmail.com>
2026-09-24 10:02:42 +0530

    Updated

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -114,13 +114,13 @@ const Navbar: React.FC = () => {
 
   return (
     <>
-    {showOffer && (
-        <TopOfferBar
-          onClose={() => setShowOffer(false)}
-        />
+      {showOffer && (
+        <div className="fixed top-0 left-0 w-full z-[60]">
+          <TopOfferBar onClose={() => setShowOffer(false)} />
+        </div>
       )}
-    <nav
-      className={`
+      <nav
+        className={`
         w-full
         bg-white
         fixed
@@ -134,269 +134,269 @@ const Navbar: React.FC = () => {
         duration-300
         ${showOffer ? "top-[44px]" : "top-0"}
       `}
-    >
-      <div className="h-full max-w-7xl mx-auto flex justify-between items-center">
-        {/* Logo */}
-        <Link to="/" className="h-full flex items-center">
-          <img
-            src="https://etrain.blr1.cdn.digitaloceanspaces.com/logo.gif"
-            alt="EtrainIndia"
-            className="h-12 sm:h-14 object-contain"
-          />
-        </Link>
-
-        {/* Courses Dropdown */}
-        <div
-          className="relative group hidden md:block"
-          onMouseEnter={() => setIsDropdownOpen(true)}
-          onMouseLeave={() => setIsDropdownOpen(false)}
-        >
-          <button className="px-4 py-2 font-medium rounded-md border border-transparent hover:border-[#008641] hover:text-[#008641] transition duration-200">
-            Courses | Certifications
-          </button>
-          {isDropdownOpen && <DropdownWrapper />}
-        </div>
-
-        {/* Desktop Search */}
-        <div className="hidden md:flex gap-4 items-center">
-          <form
-            onSubmit={onSubmitSearch}
-            className="flex flex-row relative gap-4"
-          >
-            <Input
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-              onFocus={() =>
-                query.length >= 2 &&
-                setIsSuggestionsOpen(suggestions.length > 0)
-              }
-              onBlur={() => {
-                // delay closing to allow click
-                setTimeout(() => setIsSuggestionsOpen(false), 150);
-              }}
-              type="text"
-              placeholder="Search Courses..."
-              className="border rounded-md p-2 hover:ring-2 hover:ring-[#008641] focus:ring-2 focus:ring-[#008641] focus:outline-none transition duration-200"
+      >
+        <div className="h-full max-w-7xl mx-auto flex justify-between items-center">
+          {/* Logo */}
+          <Link to="/" className="h-full flex items-center">
+            <img
+              src="https://etrain.blr1.cdn.digitaloceanspaces.com/logo.gif"
+              alt="EtrainIndia"
+              className="h-12 sm:h-14 object-contain"
             />
-            <Button
-              type="submit"
-              variant="ghost"
-              size="icon"
-              onClick={() => onSubmitSearch()}
-              className="hover:ring-2 hover:ring-[#008641] focus:ring-2 focus:ring-[#008641] focus:outline-none transition duration-200 hover:bg-transparent"
-            >
-              <Search className="h-6 w-6" />
-            </Button>
+          </Link>
 
-            {isSuggestionsOpen && (
-              <SuggestionDropdown
-                items={suggestions}
-                onSelect={(val) => onSuggestionClick(val)}
+          {/* Courses Dropdown */}
+          <div
+            className="relative group hidden md:block"
+            onMouseEnter={() => setIsDropdownOpen(true)}
+            onMouseLeave={() => setIsDropdownOpen(false)}
+          >
+            <button className="px-4 py-2 font-medium rounded-md border border-transparent hover:border-[#008641] hover:text-[#008641] transition duration-200">
+              Courses | Certifications
+            </button>
+            {isDropdownOpen && <DropdownWrapper />}
+          </div>
+
+          {/* Desktop Search */}
+          <div className="hidden md:flex gap-4 items-center">
+            <form
+              onSubmit={onSubmitSearch}
+              className="flex flex-row relative gap-4"
+            >
+              <Input
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onFocus={() =>
+                  query.length >= 2 &&
+                  setIsSuggestionsOpen(suggestions.length > 0)
+                }
+                onBlur={() => {
+                  // delay closing to allow click
+                  setTimeout(() => setIsSuggestionsOpen(false), 150);
+                }}
+                type="text"
+                placeholder="Search Courses..."
+                className="border rounded-md p-2 hover:ring-2 hover:ring-[#008641] focus:ring-2 focus:ring-[#008641] focus:outline-none transition duration-200"
               />
-            )}
-          </form>
-        </div>
+              <Button
+                type="submit"
+                variant="ghost"
+                size="icon"
+                onClick={() => onSubmitSearch()}
+                className="hover:ring-2 hover:ring-[#008641] focus:ring-2 focus:ring-[#008641] focus:outline-none transition duration-200 hover:bg-transparent"
+              >
+                <Search className="h-6 w-6" />
+              </Button>
 
-        {/* Right-side */}
-        <div className="flex items-center gap-3 md:gap-4">
-          <Link
-            to="/"
-            className="hidden md:flex text-sm sm:text-base font-medium hover:text-[#008641]"
-          >
-            Home
-          </Link>
-          <Link
-            to="/partnerwithus"
-            className="hidden md:flex text-sm sm:text-base font-medium hover:text-[#008641]"
-          >
-            Partner with Us
-          </Link>
+              {isSuggestionsOpen && (
+                <SuggestionDropdown
+                  items={suggestions}
+                  onSelect={(val) => onSuggestionClick(val)}
+                />
+              )}
+            </form>
+          </div>
 
-          <Link to="/cart">
-            <Button
-              variant="ghost"
-              size="icon"
-              className="hover:ring-2 hover:ring-[#008641] focus:ring-2 focus:ring-[#008641] focus:outline-none transition duration-200 hover:bg-transparent cursor-pointer"
+          {/* Right-side */}
+          <div className="flex items-center gap-3 md:gap-4">
+            <Link
+              to="/"
+              className="hidden md:flex text-sm sm:text-base font-medium hover:text-[#008641]"
             >
-              <ShoppingCart className="h-6 w-6" />
-            </Button>
-          </Link>
-
-          {/* Profile Dropdown (on hover) */}
-          {isAuthenticated ? (
-            <div
-              className="relative"
-              onMouseEnter={() => setIsProfileDropdownOpen(true)}
-              onMouseLeave={() => setIsProfileDropdownOpen(false)}
+              Home
+            </Link>
+            <Link
+              to="/partnerwithus"
+              className="hidden md:flex text-sm sm:text-base font-medium hover:text-[#008641]"
             >
+              Partner with Us
+            </Link>
+
+            <Link to="/cart">
               <Button
                 variant="ghost"
                 size="icon"
-                className="hover:ring-2 hover:ring-[#008641] focus:ring-2 cursor-pointer focus:ring-[#008641] hover:bg-transparent"
+                className="hover:ring-2 hover:ring-[#008641] focus:ring-2 focus:ring-[#008641] focus:outline-none transition duration-200 hover:bg-transparent cursor-pointer"
               >
-                <User className="h-6 w-6 text-[#008641]" />
+                <ShoppingCart className="h-6 w-6" />
               </Button>
+            </Link>
 
-              {isProfileDropdownOpen && (
-                <div className="absolute right-0 w-40 bg-white mt-[1px] rounded-lg shadow-lg border border-gray-100 p-2 flex flex-col z-50">
-                  <Button
-                    variant="ghost"
-                    className="flex items-center gap-2 text-sm justify-start cursor-pointer hover:bg-gray-100"
-                    onClick={() => {
-                      navigate("/dashboard");
-                      setIsProfileDropdownOpen(false);
-                    }}
-                  >
-                    <LayoutDashboard className="h-4 w-4" /> Dashboard
-                  </Button>
-                  <Button
-                    variant="ghost"
-                    className="flex items-center gap-2 text-sm justify-start hover:bg-gray-100 cursor-pointer text-red-600"
-                    onClick={handleLogout}
-                  >
-                    <LogOut className="h-4 w-4" /> Logout
-                  </Button>
+            {/* Profile Dropdown (on hover) */}
+            {isAuthenticated ? (
+              <div
+                className="relative"
+                onMouseEnter={() => setIsProfileDropdownOpen(true)}
+                onMouseLeave={() => setIsProfileDropdownOpen(false)}
+              >
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="hover:ring-2 hover:ring-[#008641] focus:ring-2 cursor-pointer focus:ring-[#008641] hover:bg-transparent"
+                >
+                  <User className="h-6 w-6 text-[#008641]" />
+                </Button>
+
+                {isProfileDropdownOpen && (
+                  <div className="absolute right-0 w-40 bg-white mt-[1px] rounded-lg shadow-lg border border-gray-100 p-2 flex flex-col z-50">
+                    <Button
+                      variant="ghost"
+                      className="flex items-center gap-2 text-sm justify-start cursor-pointer hover:bg-gray-100"
+                      onClick={() => {
+                        navigate("/dashboard");
+                        setIsProfileDropdownOpen(false);
+                      }}
+                    >
+                      <LayoutDashboard className="h-4 w-4" /> Dashboard
+                    </Button>
+                    <Button
+                      variant="ghost"
+                      className="flex items-center gap-2 text-sm justify-start hover:bg-gray-100 cursor-pointer text-red-600"
+                      onClick={handleLogout}
+                    >
+                      <LogOut className="h-4 w-4" /> Logout
+                    </Button>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <Button
+                onClick={openAuth}
+                variant="outline"
+                className="hidden md:flex items-center gap-2 hover:ring-2 hover:ring-[#008641] focus:ring-2 focus:ring-[#008641] focus:outline-none transition duration-200 hover:bg-transparent hover:text-inherit border-[#e5e7eb] cursor-pointer"
+              >
+                <User className="h-5 w-5" /> Login / Signup
+              </Button>
+            )}
+
+            {/* Mobile Menu Toggle */}
+            <Button
+              variant="ghost"
+              size="icon"
+              className="md:hidden"
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            >
+              {isMobileMenuOpen ? (
+                <X className="h-6 w-6" />
+              ) : (
+                <Menu className="h-6 w-6" />
+              )}
+            </Button>
+          </div>
+        </div>
+
+        {/* Mobile Dropdown Menu */}
+        {isMobileMenuOpen && (
+          <div className="md:hidden mt-2 bg-white rounded-md shadow-md px-4 py-4 space-y-3">
+            <Link
+              to="/"
+              onClick={() => setIsMobileMenuOpen(false)}
+              className="block text-base font-medium"
+            >
+              Home
+            </Link>
+            <Link
+              to="/aboutus"
+              onClick={() => setIsMobileMenuOpen(false)}
+              className="block text-base font-medium"
+            >
+              About
+            </Link>
+            <Link
+              to="/partnerwithus"
+              onClick={() => setIsMobileMenuOpen(false)}
+              className="block text-base font-medium"
+            >
+              Partner with Us
+            </Link>
+            <div>
+              <button
+                onClick={() => setIsCoursesOpen(!isCoursesOpen)}
+                className="w-full flex items-center justify-between text-base font-medium"
+              >
+                <span>Courses | Certifications</span>
+
+                <svg
+                  className={`w-4 h-4 transition-transform ${
+                    isCoursesOpen ? "rotate-180" : ""
+                  }`}
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M19 9l-7 7-7-7"
+                  />
+                </svg>
+              </button>
+
+              {isCoursesOpen && (
+                <div className="mt-3 ml-2 border-l-2 border-[#008641] pl-3">
+                  <MobileCourseDropdown />
                 </div>
               )}
             </div>
-          ) : (
+            <Link
+              to="/contactus"
+              onClick={() => setIsMobileMenuOpen(false)}
+              className="block text-base font-medium"
+            >
+              Contact Us
+            </Link>
+            <div className="relative w-full">
+              <form
+                onSubmit={onSubmitSearch}
+                className="flex items-center gap-2"
+              >
+                <Input
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                  onFocus={() =>
+                    query.length >= 2 &&
+                    setIsSuggestionsOpen(suggestions.length > 0)
+                  }
+                  onBlur={() => {
+                    setTimeout(() => setIsSuggestionsOpen(false), 150);
+                  }}
+                  type="text"
+                  placeholder="Search Courses..."
+                  className="w-full border rounded-md p-2 hover:ring-2 hover:ring-[#008641] focus:ring-2 focus:ring-[#008641] focus:outline-none transition duration-200"
+                />
+
+                <Button
+                  type="submit"
+                  variant="ghost"
+                  size="icon"
+                  onClick={() => onSubmitSearch()}
+                  className="hover:ring-2 hover:ring-[#008641] focus:ring-2 focus:ring-[#008641] focus:outline-none transition duration-200 hover:bg-transparent"
+                >
+                  <Search className="h-5 w-5" />
+                </Button>
+              </form>
+
+              {isSuggestionsOpen && (
+                <SuggestionDropdown
+                  items={suggestions}
+                  onSelect={(val) => onSuggestionClick(val)}
+                />
+              )}
+            </div>
             <Button
-              onClick={openAuth}
+              onClick={() => {
+                setIsMobileMenuOpen(false);
+                openAuth();
+              }}
               variant="outline"
-              className="hidden md:flex items-center gap-2 hover:ring-2 hover:ring-[#008641] focus:ring-2 focus:ring-[#008641] focus:outline-none transition duration-200 hover:bg-transparent hover:text-inherit border-[#e5e7eb] cursor-pointer"
+              className="w-full flex items-center justify-center gap-2 cursor-pointer"
             >
               <User className="h-5 w-5" /> Login / Signup
             </Button>
-          )}
-
-          {/* Mobile Menu Toggle */}
-          <Button
-            variant="ghost"
-            size="icon"
-            className="md:hidden"
-            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-          >
-            {isMobileMenuOpen ? (
-              <X className="h-6 w-6" />
-            ) : (
-              <Menu className="h-6 w-6" />
-            )}
-          </Button>
-        </div>
-      </div>
-
-      {/* Mobile Dropdown Menu */}
-      {isMobileMenuOpen && (
-        <div className="md:hidden mt-2 bg-white rounded-md shadow-md px-4 py-4 space-y-3">
-          <Link
-            to="/"
-            onClick={() => setIsMobileMenuOpen(false)}
-            className="block text-base font-medium"
-          >
-            Home
-          </Link>
-          <Link
-            to="/aboutus"
-            onClick={() => setIsMobileMenuOpen(false)}
-            className="block text-base font-medium"
-          >
-            About
-          </Link>
-          <Link
-            to="/partnerwithus"
-            onClick={() => setIsMobileMenuOpen(false)}
-            className="block text-base font-medium"
-          >
-            Partner with Us
-          </Link>
-          <div>
-            <button
-              onClick={() => setIsCoursesOpen(!isCoursesOpen)}
-              className="w-full flex items-center justify-between text-base font-medium"
-            >
-              <span>Courses | Certifications</span>
-
-              <svg
-                className={`w-4 h-4 transition-transform ${
-                  isCoursesOpen ? "rotate-180" : ""
-                }`}
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M19 9l-7 7-7-7"
-                />
-              </svg>
-            </button>
-
-            {isCoursesOpen && (
-              <div className="mt-3 ml-2 border-l-2 border-[#008641] pl-3">
-                <MobileCourseDropdown />
-              </div>
-            )}
           </div>
-          <Link
-            to="/contactus"
-            onClick={() => setIsMobileMenuOpen(false)}
-            className="block text-base font-medium"
-          >
-            Contact Us
-          </Link>
-          <div className="relative w-full">
-  <form
-    onSubmit={onSubmitSearch}
-    className="flex items-center gap-2"
-  >
-    <Input
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
-      onFocus={() =>
-        query.length >= 2 &&
-        setIsSuggestionsOpen(suggestions.length > 0)
-      }
-      onBlur={() => {
-        setTimeout(() => setIsSuggestionsOpen(false), 150);
-      }}
-      type="text"
-      placeholder="Search Courses..."
-      className="w-full border rounded-md p-2 hover:ring-2 hover:ring-[#008641] focus:ring-2 focus:ring-[#008641] focus:outline-none transition duration-200"
-    />
-
-    <Button
-      type="submit"
-      variant="ghost"
-      size="icon"
-      onClick={() => onSubmitSearch()}
-      className="hover:ring-2 hover:ring-[#008641] focus:ring-2 focus:ring-[#008641] focus:outline-none transition duration-200 hover:bg-transparent"
-    >
-      <Search className="h-5 w-5" />
-    </Button>
-  </form>
-
-  {isSuggestionsOpen && (
-    <SuggestionDropdown
-      items={suggestions}
-      onSelect={(val) => onSuggestionClick(val)}
-    />
-  )}
-</div>
-          <Button
-            onClick={() => {
-              setIsMobileMenuOpen(false);
-              openAuth();
-            }}
-            variant="outline"
-            className="w-full flex items-center justify-center gap-2 cursor-pointer"
-          >
-            <User className="h-5 w-5" /> Login / Signup
-          </Button>
-        </div>
-      )}
-    </nav>
+        )}
+      </nav>
     </>
   );
 };
